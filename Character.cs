c5bb979a555863c13454abf3f8c427e6bec5618e
magemi <magemi@pm.me>
2023-08-30 13:31:35 -0400

    Implemented a* pathfinding algorithm

--- Character.cs
+++ Character.cs
@@ -1,38 +1,102 @@
 using Godot;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Character : Node2D
 {
 	[Export]
 	public float Speed { get; set; } = 200.0f;
+
+	private const float Mass = 10;
+	private const float DistanceToArrival = 10;
 	
 	private Vector2 _velocity = Vector2.Zero;
 	public Vector2 _target;
-	
+	private Vector2 _nextPoint = Vector2.Zero;
+	private List<Vector2> _path;
+	private PathFinder _pathFinder = new PathFinder();
+
+	public enum CharacterState { Idle, Searching, Moving };
+	public CharacterState State { get; set; } = CharacterState.Idle;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_target = Position;
+		_pathFinder = GetNode<PathFinder>("../TileMap");
+		ChangeState(CharacterState.Idle);
 	}
-
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (_target != Position)
+		if (State != CharacterState.Moving)
+			return;
+
+		var arrivedToNextPoint = MoveTo(_nextPoint);
+		if (arrivedToNextPoint)
 		{
-			Rotation = _target.AngleToPoint(Position) + Mathf.Pi;
-			Position = Position.MoveToward(_target, (float)delta * Speed);
-		}	
+			_path.RemoveAt(0);
+			if (!_path.Any())
+			{
+				ChangeState(CharacterState.Idle);
+				return;
+			}
+			_nextPoint = _path[0];
+		}
 	}
 	
 	public override void _UnhandledInput(InputEvent @event)
 	{
-		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		if (@event is InputEventMouseMotion mouseMotionEvent)
 		{
-			if (mouseEvent.ButtonIndex == MouseButton.Left)
+			_target = GetGlobalMousePosition();
+			if (_pathFinder.IsPointWalkable(_target) && State != CharacterState.Moving)
 			{
-				_target = GetGlobalMousePosition();
+				ChangeState(CharacterState.Searching);
+				QueueRedraw();
 			}
 		}
+		if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.Pressed)
+		{
+			if (mouseButtonEvent.ButtonIndex == MouseButton.Left)
+			{
+				_target = GetGlobalMousePosition();
+				var label = GetParent().GetNode<Label>("Label");
+				label.Text = _target.ToString();
+				GD.Print(_target.ToString());
+				if (_pathFinder.IsPointWalkable(_target))
+					ChangeState(CharacterState.Moving);
+			}
+		}
+	}
+
+	private void ChangeState(CharacterState state)
+	{
+		if (state == CharacterState.Idle)
+		{
+			_pathFinder.ClearPath();
+		}
+		else if (state == CharacterState.Searching || state == CharacterState.Moving)
+		{
+			_path = _pathFinder.FindPath((Vector2I)Position, (Vector2I)_target).ToList();
+			if (_path.Count < 2)
+			{
+				ChangeState(CharacterState.Idle);
+				return;
+			}
+			_nextPoint = _path[0];
+		}
+		State = state;
+	}
+
+	private bool MoveTo(Vector2 position)
+	{
+		var desiredVelocity = (position - Position).Normalized() * Speed;
+		var steering = desiredVelocity - _velocity;
+		_velocity += steering / Mass;
+		Position += new Vector2(_velocity.X * (float)GetProcessDeltaTime(), _velocity.Y * (float)GetProcessDeltaTime());
+		Rotation = _velocity.Angle();
+		return Position.DistanceTo(position) < DistanceToArrival;
 	}
 }
---
+++ PathFinder.cs
@@ -0,0 +1,92 @@
+using Godot;
+using System;
+public partial class PathFinder : TileMap
+{
+	private enum Tile { Obstacle, StartPoint, EndPoint };
+	private readonly Vector2 CellSize = new Vector2(64, 64);
+
+	private AStarGrid2D _aStar = new AStarGrid2D();
+	private Vector2I _startPoint = Vector2I.Zero;
+	private Vector2I _endPoint = Vector2I.Zero;
+	private Vector2[] _path;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_aStar.Region = GetUsedRect();
+		_aStar.CellSize = CellSize;
+		_aStar.Offset = new Vector2(CellSize.X / 2, CellSize.Y / 2);
+		_aStar.DefaultComputeHeuristic = AStarGrid2D.Heuristic.Manhattan;
+		_aStar.DefaultEstimateHeuristic = AStarGrid2D.Heuristic.Manhattan;
+		_aStar.DiagonalMode = AStarGrid2D.DiagonalModeEnum.Never;
+		_aStar.Update();
+
+		foreach (var i in GD.Range(_aStar.Region.Position.X, _aStar.Region.End.X))
+		{
+			foreach (var j in GD.Range(_aStar.Region.Position.Y, _aStar.Region.End.Y))
+			{
+				var position = new Vector2I(i, j);
+				if (GetCellSourceId(0, position) == (int)Tile.Obstacle)
+					_aStar.SetPointSolid(position);
+			}
+		}
+	}
+
+    public override void _Draw()
+    {
+        if (_path.IsEmpty())
+            return;
+
+        var lastPoint = _path[0];
+        foreach (var i in GD.Range(0, _path.Length))
+        {
+            var currentPoint = _path[i];
+            DrawLine(lastPoint, currentPoint, Colors.White, 3.0f, true);
+            DrawCircle(currentPoint, 6.0f, Colors.White);
+            lastPoint = currentPoint;
+        }
+    }
+    
+    public Vector2 RoundLocalPosition(Vector2 position)
+	{
+		return MapToLocal(LocalToMap(position));
+	}
+
+	public bool IsPointWalkable(Vector2 position)
+	{
+		var mapPosition = LocalToMap((Vector2I)ToLocal(position));
+
+		if (_aStar.IsInBoundsv(mapPosition))
+			return !_aStar.IsPointSolid(mapPosition);
+
+		return false;
+	}
+
+	public void ClearPath()
+	{
+		if (!_path.IsEmpty())
+		{
+			Array.Clear(_path);
+			EraseCell(0, _startPoint);
+			EraseCell(0, _endPoint);
+        }
+        QueueRedraw();
+    }
+
+	public Vector2[] FindPath(Vector2I startPoint, Vector2I endPoint)
+	{
+		ClearPath();
+
+		_startPoint = LocalToMap(startPoint);
+		_endPoint = LocalToMap(endPoint);
+		_path = _aStar.GetPointPath(_startPoint, _endPoint);
+
+		if (!_path.IsEmpty())
+		{
+			SetCell(0, _startPoint, (int)Tile.StartPoint, Vector2I.Zero);
+			SetCell(0, _endPoint, (int)Tile.EndPoint, Vector2I.Zero);
+		}
+
+		return (Vector2[])_path.Clone();
+	}
+}
